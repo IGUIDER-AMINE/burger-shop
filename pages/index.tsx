2c4b154ff99f44f440972ddb3a5633fac1203bcd
IGUIDER-AMINE <amineiguider2017@gmail.com>
2023-07-13 11:56:22 +0200

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,18 @@
+import Head from "next/head";
+import dynamic from "next/dynamic";
+import Layout from "@/components/layout/Layout";
+
+const Banner = dynamic(() => import("@/components/sections/Banner"));
+
 export default function Home() {
-  return <main>s</main>;
+  return (
+    <>
+      <Head>
+        <title>Burger-Shop</title>
+      </Head>
+      <Layout>
+        <Banner />
+      </Layout>
+    </>
+  );
 }
