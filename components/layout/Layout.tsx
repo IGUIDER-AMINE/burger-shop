165342f7a176762f8c9a87c50d5729aaf0dc41a0
IGUIDER-AMINE <amineiguider2017@gmail.com>
2023-07-13 16:21:47 +0200

    push

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -12,9 +12,7 @@ const Layout = ({ children }: LayoutProps) => {
     <section>
       <main className="h-[100vh] font-bodyFont bg-gradient-to-r bg-[#FEFDF9] font-medium text-standardcolor justify-between overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60 flex flex-col">
         <Header />
-        <div className="flex flex-col justify-between gap-y-20 my-20">
-          {children}
-        </div>
+        {children}
         <Footer />
       </main>
     </section>
